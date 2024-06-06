bc4bfc3dd7b65bc6735cf3c29c1ac26727d73f44
lutfilhadi <lutfilhadiabdulrahim@gmail.com>
2024-06-07 00:59:38 +0800

    checklist date fix

--- app/(tabs)/checklist.jsx
+++ app/(tabs)/checklist.jsx
@@ -1,81 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, CheckBox, Platform, Button } from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { Ionicons } from '@expo/vector-icons/';
-import DateTimePickerModal from "react-native-modal-datetime-picker";
-import axios from 'axios';
-import * as SecureStore from 'expo-secure-store';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Platform } from 'react-native'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import DateTimePickerModal from 'react-native-modal-datetime-picker'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect } from 'react'
+import * as SecureStore from 'expo-secure-store'
+import { Ionicons } from '@expo/vector-icons/'
 import moment from 'moment-timezone'
+import axios from 'axios'
 
 const Checklists = () => {
   const [selectedDate, setSelectedDate] = useState('')
-  const [isFormOpen, setIsFormOpen] = useState(false);
-  const [newItem, setNewItem] = useState(''); 
-  const [flightRoute, setFlightRoute] = useState(''); 
-  const [travelDate, setTravelDate] = useState(''); 
-  const [showDatePicker, setShowDatePicker] = useState(false); 
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [newItemText, setNewItemText] = useState('');
-  const [checklistItemOptions, setChecklistItemOptions] = useState([]); 
+  const [isFormOpen, setIsFormOpen] = useState(false)
+  const [newItem, setNewItem] = useState('')
+  const [flightRoute, setFlightRoute] = useState('')
+  const [travelDate, setTravelDate] = useState('')
+  const [showDatePicker, setShowDatePicker] = useState(false)
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false)
+  const [newItemText, setNewItemText] = useState('')
+  const [checklistItemOptions, setChecklistItemOptions] = useState([])
 
   const handleOpenForm = () => {
-    setIsFormOpen(true);
-  };
+    setIsFormOpen(true)
+  }
 
   const handleCloseForm = () => {
-    setIsFormOpen(false);
-    setNewItem('');
-    setFlightRoute('');
-    setTravelDate(new Date());
-    setChecklistItemOptions([]); 
-  };
+    setIsFormOpen(false)
+    setNewItem('')
+    setFlightRoute('')
+    setTravelDate(new Date())
+    setChecklistItemOptions([])
+  }
 
-  const handleItemChange = (text) => {
-    setNewItem(text);
-  };
+  const handleItemChange = text => {
+    setNewItem(text)
+  }
 
-  const handleFlightRouteChange = (text) => {
-    setFlightRoute(text);
-  };
+  const handleFlightRouteChange = text => {
+    setFlightRoute(text)
+  }
 
   const displayDatePicker = () => {
-    setDatePickerVisibility(true);
-  };
+    setDatePickerVisibility(true)
+  }
 
   const hideDatePicker = () => {
-    setDatePickerVisibility(false);
-  };
+    setDatePickerVisibility(false)
+  }
 
-  const handleConfirm = (date) => {
+  const handleConfirm = date => {
     const formattedTravelDate = moment(date).format('YYYY-MM-DD')
     setTravelDate(formattedTravelDate)
-    console.warn("A date has been picked: ", date);
-    hideDatePicker();
-  };
+    hideDatePicker()
+  }
 
   const handleTravelDateChange = (event, selectedDate) => {
-    const currentDate = selectedDate || travelDate;
-    setShowDatePicker(Platform.OS === 'ios');
-    setTravelDate(currentDate);
-  };
+    const currentDate = selectedDate || travelDate
+    setShowDatePicker(Platform.OS === 'ios')
+    setTravelDate(currentDate)
+  }
 
-  const handleAddChecklistItem = (text) => {
+  const handleAddChecklistItem = text => {
     if (text.trim() && checklistItemOptions.length < 20) {
-      setChecklistItemOptions([...checklistItemOptions, text.trim()]);
-      setNewItemText('');
+      setChecklistItemOptions([...checklistItemOptions, text.trim()])
+      setNewItemText('')
     }
-  };
+  }
 
-  const handleRemoveChecklistItem = (index) => {
-    const updatedItems = [...checklistItemOptions];
-    updatedItems.splice(index, 1);
-    setChecklistItemOptions(updatedItems);
-  };
+  const handleRemoveChecklistItem = index => {
+    const updatedItems = [...checklistItemOptions]
+    updatedItems.splice(index, 1)
+    setChecklistItemOptions(updatedItems)
+  }
 
-  const leftColumnItems = checklistItemOptions.slice(0, 10);
-  const rightColumnItems = checklistItemOptions.slice(10, 20);
-  const isLimitReached = checklistItemOptions.length >= 20;
+  const leftColumnItems = checklistItemOptions.slice(0, 10)
+  const rightColumnItems = checklistItemOptions.slice(10, 20)
+  const isLimitReached = checklistItemOptions.length >= 20
 
   const handleCreateChecklist = async () => {
     const userId = await SecureStore.getItemAsync('userId')
@@ -88,14 +87,13 @@ const Checklists = () => {
     }
     try {
       const response = await axios.post('https://2778-183-171-133-92.ngrok-free.app/api/checklists', checklistData)
-    
-      console.log('Checklist created:', response.data);
+      console.log('Checklist created:', response.data)
       // Reset form fields after successful creation
-      handleCloseForm();
+      handleCloseForm()
     } catch (error) {
       console.error('Error saving event:', error)
     }
-  };
+  }
 
   return (
     <View style={styles.container}>
@@ -131,8 +129,8 @@ const Checklists = () => {
         />
       )}
     </View>
-  );
-};
+  )
+}
 
 const CreateItemChecklistForm = ({
   newItem,
@@ -157,10 +155,9 @@ const CreateItemChecklistForm = ({
   rightColumnItems,
   isLimitReached,
   onClose,
-  onCreate,
+  onCreate
 }) => {
-  const getLocalTime = (time) => {
-    console.log(time)
+  const getLocalTime = time => {
     return moment(time).format('DD/MM/YYYY')
   }
 
@@ -170,124 +167,119 @@ const CreateItemChecklistForm = ({
       <View style={formStyles.box}>
         <View style={formStyles.inputBox}>
           <TextInput
-              style={formStyles.title}
-              placeholder="Enter title..."
-              placeholderTextColor="grey"
-              value={newItem}
-              onChangeText={onItemChange}
+            style={formStyles.title}
+            placeholder="Enter title..."
+            placeholderTextColor="grey"
+            value={newItem}
+            onChangeText={onItemChange}
           />
-          <Ionicons name="pencil" size={18} color="#000"/>
+          <Ionicons name="pencil" size={18} color="#000" />
         </View>
         <View style={formStyles.inputDetails}>
           <TextInput
-              style={formStyles.details}
-              placeholder="Enter flight route..."
-              placeholderTextColor="grey"
-              value={flightRoute}
-              onChangeText={onFlightRouteChange}
+            style={formStyles.details}
+            placeholder="Enter flight route..."
+            placeholderTextColor="grey"
+            value={flightRoute}
+            onChangeText={onFlightRouteChange}
           />
-          <Ionicons name="pencil" size={18} color="#000"/>
+          <Ionicons name="pencil" size={18} color="#000" />
         </View>
-          <View style={formStyles.inputDetails}>
-            <TouchableOpacity
-              onPress={displayDatePicker}
-              style={formStyles.details}
-            >
-              <Text style={[formStyles.dateText, travelDate ? {} : { color: 'grey' }]}>
-                {travelDate 
-                ? `${getLocalTime(travelDate)}` 
-                : 'Select travel date   '}
-              </Text>
-            </TouchableOpacity>
-            <DateTimePickerModal
-              isVisible={isDatePickerVisible}
-              mode="date"
-              onConfirm={handleConfirm}
-              onCancel={hideDatePicker}
-            />
-            <Ionicons name="pencil" size={18} color="#000"/>
-          </View>
-          <View>
-            <Text style={formStyles.checklistTitle}>Essential</Text>
-            <View style={{ flexDirection: 'row' }}>
-              <View style={{ flex: 1 }}>
-                {leftColumnItems.map((option, index) => (
-                  <View key={index} style={formStyles.checklistItem}>
-                    <Text style={formStyles.checklistItemNumber}>{index + 1}.</Text>
-                    <Text style={formStyles.checklistItemText}>{option}</Text>
-                    <TouchableOpacity onPress={() => onRemoveChecklistItem(index)}>
-                      <Ionicons name="close" size={16} color="red" />
-                    </TouchableOpacity>
-                  </View>
-                ))}
-                {!isLimitReached && leftColumnItems.length < 10 && (
-                  <View style={formStyles.inputDetails}>
-                    <Ionicons name="add-circle" size={18} color="grey" />
-                    <TextInput
-                      style={[formStyles.input, { fontStyle: 'normal' }]}
-                      placeholder="Add item..."
-                      placeholderTextColor="grey"
-                      onChangeText={(text) => setNewItemText(text)}
-                      onSubmitEditing={() => {
-                        onAddChecklistItem(newItemText);
-                        setNewItemText('');
-                      }}
-                      value={newItemText}
-                    />
-                  </View>
-                )}
-              </View>
-              <View style={{ flex: 1 }}>
-                {rightColumnItems.map((option, index) => (
-                  <View key={index + 10} style={formStyles.checklistItem}>
-                    <Text style={formStyles.checklistItemNumber}>{index + 11}.</Text>
-                    <Text style={formStyles.checklistItemText}>{option}</Text>
-                    <TouchableOpacity onPress={() => onRemoveChecklistItem(index + 10)}>
-                      <Ionicons name="close" size={16} color="red" />
-                    </TouchableOpacity>
-                  </View>
-                ))}
-                {!isLimitReached && rightColumnItems.length < 10 && leftColumnItems.length === 10 && (
-                  <View style={formStyles.inputDetails}>
-                    <Ionicons name="add-circle" size={18} color="grey" />
-                    <TextInput
-                      style={[formStyles.input, { fontStyle: 'normal' }]}
-                      placeholder="Add item..."
-                      placeholderTextColor="grey"
-                      onChangeText={(text) => setNewItemText(text)}
-                      onSubmitEditing={() => {
-                        onAddChecklistItem(newItemText);
-                        setNewItemText('');
-                      }}
-                      value={newItemText}
-                    />
-                  </View>
-                )}
-              </View>
+        <View style={formStyles.inputDetails}>
+          <TouchableOpacity onPress={displayDatePicker} style={formStyles.details}>
+            <Text style={[formStyles.dateText, travelDate ? {} : { color: 'grey' }]}>
+              {travelDate ? `${getLocalTime(travelDate)}` : 'Select travel date   '}
+            </Text>
+          </TouchableOpacity>
+          <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="date"
+            onConfirm={handleConfirm}
+            onCancel={hideDatePicker}
+          />
+          <Ionicons name="pencil" size={18} color="#000" />
+        </View>
+        <View>
+          <Text style={formStyles.checklistTitle}>Essential</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flex: 1 }}>
+              {leftColumnItems.map((option, index) => (
+                <View key={index} style={formStyles.checklistItem}>
+                  <Text style={formStyles.checklistItemNumber}>{index + 1}.</Text>
+                  <Text style={formStyles.checklistItemText}>{option}</Text>
+                  <TouchableOpacity onPress={() => onRemoveChecklistItem(index)}>
+                    <Ionicons name="close" size={16} color="red" />
+                  </TouchableOpacity>
+                </View>
+              ))}
+              {!isLimitReached && leftColumnItems.length < 10 && (
+                <View style={formStyles.inputDetails}>
+                  <Ionicons name="add-circle" size={18} color="grey" />
+                  <TextInput
+                    style={[formStyles.input, { fontStyle: 'normal' }]}
+                    placeholder="Add item..."
+                    placeholderTextColor="grey"
+                    onChangeText={text => setNewItemText(text)}
+                    onSubmitEditing={() => {
+                      onAddChecklistItem(newItemText)
+                      setNewItemText('')
+                    }}
+                    value={newItemText}
+                  />
+                </View>
+              )}
+            </View>
+            <View style={{ flex: 1 }}>
+              {rightColumnItems.map((option, index) => (
+                <View key={index + 10} style={formStyles.checklistItem}>
+                  <Text style={formStyles.checklistItemNumber}>{index + 11}.</Text>
+                  <Text style={formStyles.checklistItemText}>{option}</Text>
+                  <TouchableOpacity onPress={() => onRemoveChecklistItem(index + 10)}>
+                    <Ionicons name="close" size={16} color="red" />
+                  </TouchableOpacity>
+                </View>
+              ))}
+              {!isLimitReached && rightColumnItems.length < 10 && leftColumnItems.length === 10 && (
+                <View style={formStyles.inputDetails}>
+                  <Ionicons name="add-circle" size={18} color="grey" />
+                  <TextInput
+                    style={[formStyles.input, { fontStyle: 'normal' }]}
+                    placeholder="Add item..."
+                    placeholderTextColor="grey"
+                    onChangeText={text => setNewItemText(text)}
+                    onSubmitEditing={() => {
+                      onAddChecklistItem(newItemText)
+                      setNewItemText('')
+                    }}
+                    value={newItemText}
+                  />
+                </View>
+              )}
             </View>
           </View>
-          <View style={formStyles.buttonContainer}>
-            <TouchableOpacity style={formStyles.cancelButton} onPress={onClose}>
-              <Text style={formStyles.cancelText}>Cancel</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={formStyles.createButton} onPress={onCreate}>
-              <Text style={formStyles.createText}>Create</Text>
-            </TouchableOpacity>
-          </View>
+        </View>
+        <View style={formStyles.buttonContainer}>
+          <TouchableOpacity style={formStyles.cancelButton} onPress={onClose}>
+            <Text style={formStyles.cancelText}>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={formStyles.createButton} onPress={onCreate}>
+            <Text style={formStyles.createText}>Create</Text>
+          </TouchableOpacity>
         </View>
       </View>
-    );
-  };
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    position: 'relative',
+    position: 'relative'
   },
   header: {
     fontSize: 18,
     fontWeight: '600',
-    padding: 15,
+    padding: 15
   },
   button: {
     position: 'absolute',
@@ -299,16 +291,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     bottom: 30,
     right: 30,
-    elevation: 5,
-  },
-});
+    elevation: 5
+  }
+})
 
 const formStyles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     borderRadius: 10,
     position: 'relative',
-    margin: 10,
+    margin: 10
   },
   box: {
     backgroundColor: '#F8FAFC',
@@ -320,7 +312,7 @@ const formStyles = StyleSheet.create({
     padding: 10,
     marginBottom: 20,
     shadowOpacity: 0.25,
-    shadowOffset: {width: 2, height: 4},
+    shadowOffset: { width: 2, height: 4 }
   },
   header: {
     fontSize: 18,
@@ -328,14 +320,14 @@ const formStyles = StyleSheet.create({
     paddingHorizontal: 30,
     paddingVertical: 15
   },
-  title:{
+  title: {
     marginBottom: 10,
     paddingBottom: 3,
     borderBottomWidth: 1,
     borderBottomColor: '#5F5F5F',
     fontSize: 12
   },
-  details:{
+  details: {
     marginBottom: 10,
     paddingBottom: 3,
     borderBottomWidth: 1,
@@ -343,17 +335,12 @@ const formStyles = StyleSheet.create({
     fontSize: 12
   },
   inputBox: {
-    // flex: 1,
     flexDirection: 'row',
-    // justifyContent: 'center',
-    alignItems: 'stretch',
-    // marginRight: 10,
+    alignItems: 'stretch'
   },
   inputDetails: {
     flexDirection: 'row',
-    // justifyContent: 'center',
-    alignItems: 'stretch',
-    // marginRight: 10,
+    alignItems: 'stretch'
   },
   inputContainer: {
     flexDirection: 'column',
@@ -362,11 +349,11 @@ const formStyles = StyleSheet.create({
   input: {
     paddingLeft: 8,
     width: '80%',
-    fontSize: 12,
+    fontSize: 12
   },
   dateButton: {
     backgroundColor: '#045D91',
-    borderRadius: 5,
+    borderRadius: 5
   },
   datePicker: {
     flexDirection: 'row',
@@ -401,7 +388,7 @@ const formStyles = StyleSheet.create({
   checklistItem: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 5,
+    marginBottom: 5
   },
   checklistItemText: {
     marginLeft: 10,
@@ -410,12 +397,12 @@ const formStyles = StyleSheet.create({
   removeIcon: {
     fontSize: 18,
     color: '#ccc',
-    marginLeft: 'auto',
+    marginLeft: 'auto'
   },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginTop: 20,
+    marginTop: 20
   },
   createButton: {
     backgroundColor: '#045D91',
@@ -435,15 +422,14 @@ const formStyles = StyleSheet.create({
   },
   createText: {
     color: '#fff',
-    fontWeight: 600,
-    textAlign: 'center',
+    fontWeight: '600',
+    textAlign: 'center'
   },
   cancelText: {
     color: '#656565',
-    fontWeight: 600,
-    textAlign: 'center',
-  },
-});
+    fontWeight: '600',
+    textAlign: 'center'
+  }
+})
 
-export default Checklists; 
-
+export default Checklists
