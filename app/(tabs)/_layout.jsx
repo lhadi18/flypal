11f66f676f8b4ea1df402475c7b82df4610ae11a
lutfilhadi <lutfilhadiabdulrahim@gmail.com>
2024-06-10 13:23:38 +0800

    bottom nav bar fix

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,5 +1,4 @@
 // _layout.jsx
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import { useNavigation } from 'expo-router'
@@ -9,8 +8,8 @@ import React from 'react'
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center', paddingBottom: 3 }}>
-      <Image source={icon} resizeMode="contain" tintColor={color} className="w-6 h-6" style={{ height: 24, width: 24 }}/>
+    <View className="items-center justify-center gap-1">
+      <Image source={icon} resizeMode="contain" tintColor={color} className="w-6 h-6" />
       <Text
         style={{ color: focused ? '#007BFF' : '#000', fontWeight: focused ? '600' : '400', fontSize: 12, color: color }}
       >
@@ -35,6 +34,7 @@ const TabsLayout = () => {
           tabBarStyle: {
             backgroundColor: '#045D91',
             borderTopWidth: 1,
+            paddingBottom: 18,
             height: 84
           }
         }}
